e2e7196ced837141f723a18dd509a7c2fe227059
표수경 <sukyeongpyo@gmail.com>
2025-11-16 15:10:11 +0900

    feat : check input files path

--- utils/setting.py
+++ utils/setting.py
@@ -1,2 +1,6 @@
-def print_hello():
-    print("Hello")
+import os
+
+def check_inputfile():
+    for dirname, _, filenames in os.walk('/kaggle/input'):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
